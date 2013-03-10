25e128c8a377a47ebd1899f45174b443d821e4b9
Alexis Galbourdin <i@alexisg.net>
2013-03-10 17:23:52 +0100

    Automatic installation of More packages with Composer scripts.

---
+++ src/Agl/Core/Composer.php
@@ -0,0 +1,67 @@
+<?php
+namespace Agl\Core;
+
+use \Exception,
+    \Composer\Script\PackageEvent;
+
+/**
+ * Composer events methods. Provide automatic installation of More packages.
+ *
+ * @category Agl_Core
+ * @package Agl_Core
+ * @version 0.1.0
+ */
+
+class Composer
+{
+    /**
+     * AGL setup directory.
+     */
+    const SETUP_DIR = 'agl-setup/';
+
+    /**
+     * AGL install file.
+     */
+    const INSTALL_FILE = 'install.json';
+
+    /**
+     * Post package install event, fired by Composer.
+     * Create config directories and files in application if required by
+     * the package.
+     *
+     * @param PackageEvent $pEvent
+     * @return bool
+     */
+    public static function postPackageInstall(PackageEvent $pEvent)
+    {
+        $package    = $pEvent->getOperation()->getPackage()->getName();
+        $path       = realpath(
+            './'
+            . $pEvent->getComposer()->getConfig()->get('vendor-dir')
+            . '/' . $package
+            . '/' . self::SETUP_DIR
+        ) . '/';
+
+        if ($path and is_readable($path . self::INSTALL_FILE)) {
+            $installFile    = $path . self::INSTALL_FILE;
+            $installContent = json_decode(file_get_contents($installFile));
+
+            foreach ($installContent as $file => $config) {
+                $destination = realpath('./') . '/' . $config->dir;
+                $destinationFile = $destination . $config->file;
+
+                if (is_readable($destinationFile)) {
+                    return true;
+                }
+
+                if ((is_dir($destination) and ! is_writable(is_dir($destination)))
+                    or (! is_dir($destination) and ! mkdir($destination, 0777, true))
+                    or ! copy($installFile, $destinationFile)) {
+                    throw new Exception("Configuration files copy failed. Check that '$destination' has write permissions.");
+                }
+            }
+        }
+
+        return true;
+    }
+}
