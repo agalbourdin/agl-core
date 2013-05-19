090af0560f85cf75c845bc6ae9c3396d66acf3ca
i@alexisg.net <i@alexisg.net>
2013-05-19 19:17:42 +0200

    Default config files.

---
+++ agl-setup/config/acl.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+
+	'guest' => array(
+
+		'resources' => array(
+
+		),
+
+		'inherit' => array(
+
+		)
+
+	)
+
+);
---
+++ agl-setup/install.php
@@ -0,0 +1,39 @@
+<?php
+return array(
+
+	'config/main.php' => array(
+		'dir'  => 'app/etc/config/',
+		'file' => 'main.php'
+	),
+
+	'config/acl.php' => array(
+		'dir'  => 'app/etc/config/core/',
+		'file' => 'acl.php'
+	),
+
+	'config/acl.sample.php' => array(
+		'dir'  => 'app/etc/config/core/',
+		'file' => 'acl.sample.php'
+	),
+
+	'config/events.php' => array(
+		'dir'  => 'app/etc/config/core/',
+		'file' => 'events.php'
+	),
+
+	'config/events.sample.php' => array(
+		'dir'  => 'app/etc/config/core/',
+		'file' => 'events.sample.php'
+	),
+
+	'config/layout.php' => array(
+		'dir'  => 'app/etc/config/core/',
+		'file' => 'layout.php'
+	),
+
+	'config/layout.sample.php' => array(
+		'dir'  => 'app/etc/config/core/',
+		'file' => 'layout.sample.php'
+	)
+
+);
